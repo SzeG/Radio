d9cccc90fb07c2818224552bc4afdd3dd80f6614
Gergő <Gerg@DESKTOP-S2HSSE9>
2019-06-06 20:56:18 +0200

    file és lista deklarálása

---
+++ src/ZeneiEljarasok.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gergő
+ */
+public class ZeneiEljarasok {
+    
+    private String musorFile;
+    private List<ZeneiEljarasok> adas;
+
+    public ZeneiEljarasok(String musorFile) {
+        this.musorFile = musorFile;
+        this.adas = new ArrayList<>();
+    }
+    
+    
+}
