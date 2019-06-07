1e4ff46b55c5eaf136f176b31c8f558dd585eeef
Gergő <Gerg@DESKTOP-S2HSSE9>
2019-06-07 12:55:40 +0200

    OUT OF BOUNDS EXCEPTION FileReader.close; sout törlése;

--- src/javaapplication151/ZeneiEljaras.java
+++ src/javaapplication151/ZeneiEljaras.java
@@ -17,8 +17,8 @@ import java.util.List;
  * @author Gergő
  */
 public class ZeneiEljaras {
-    
-     private List<ZeneiAgy> adas;
+
+    private List<ZeneiAgy> adas;
 
     public ZeneiEljaras() {
 
@@ -28,22 +28,22 @@ public class ZeneiEljaras {
     public void betoltes() {
 
         FileReader buta;
-        String beolv;
+        String beolv = " ";
         try {
             buta = new FileReader("musor.txt");
             BufferedReader okos = new BufferedReader(buta);
-            for (int i = 0; i < Integer.MAX_VALUE; i++) {
-                beolv = okos.readLine();
+            for (int i = 0; i < 1000; i++) {
                 if (beolv == null) {
                     break;
                 }
-                String[] szamok=beolv.split(" ");
-                String[] cim=beolv.split(":");
-                String[] eloado=cim[0].split(" ");
-                ZeneiAgy za= new ZeneiAgy(Integer.parseInt(szamok[0]),(Integer.parseInt(szamok[1])*60)+(Integer.parseInt(szamok[2])),eloado[3].concat(eloado[4]),cim[1],Integer.parseInt(szamok[0]));
+                beolv = okos.readLine();
+                String[] szamok = beolv.split(" ");
+                String[] cim = beolv.split(":");
+                String[] eloado = cim[0].split(" ");
+                ZeneiAgy za = new ZeneiAgy(Integer.parseInt(szamok[0]), Integer.parseInt(szamok[1]) * 60 + Integer.parseInt(szamok[2]), eloado[3], cim[1], Integer.parseInt(szamok[0]));
                 adas.add(za);
-                System.out.println(za);
             }
+            buta.close();
         } catch (FileNotFoundException ex) {
             System.out.println("Nem található a file!");
         } catch (IOException ex) {
@@ -51,7 +51,5 @@ public class ZeneiEljaras {
         }
 
     }
-    
-   
-   
+
 }
