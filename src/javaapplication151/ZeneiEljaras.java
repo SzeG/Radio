39091e1f496caaac115644254107f61501d770ea
Gergő <Gerg@DESKTOP-S2HSSE9>
2019-06-07 07:42:14 +0200

    EZ EGY ÚJ CLASS (ugyanaz mint a régi, de a régi nem a JavaApplication151 package-ban volt...) betöltő metódust nem lehet megívni a mainból betöltő metódusban a kezdido jelenleg kamu, majd utólag lesz beállítva

---
+++ src/javaapplication151/ZeneiEljaras.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication151;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gergő
+ */
+public class ZeneiEljaras {
+    
+     private List<ZeneiAgy> adas;
+
+    public ZeneiEljaras() {
+
+        this.adas = new ArrayList<>();
+    }
+
+    public void betoltes() {
+
+        FileReader buta;
+        String beolv;
+        try {
+            buta = new FileReader("musor.txt");
+            BufferedReader okos = new BufferedReader(buta);
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                beolv = okos.readLine();
+                if (beolv == null) {
+                    break;
+                }
+                String[] szamok=beolv.split(" ");
+                String[] cim=beolv.split(":");
+                String[] eloado=cim[0].split(" ");
+                ZeneiAgy za= new ZeneiAgy(Integer.parseInt(szamok[0]),(Integer.parseInt(szamok[1])*60)+(Integer.parseInt(szamok[2])),eloado[3].concat(eloado[4]),cim[1],Integer.parseInt(szamok[0]));
+                adas.add(za);
+                System.out.println(za);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Nem található a file!");
+        } catch (IOException ex) {
+            System.out.println("Hiba a file olvasásánál!");
+        }
+
+    }
+    
+   
+   
+}
