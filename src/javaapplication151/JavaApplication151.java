7ce958d0ee6e4ca337968f9cab61e256f616a362
Gergő <Gerg@DESKTOP-S2HSSE9>
2019-06-09 12:21:53 +0200

    ze.összkiiro;

--- src/javaapplication151/JavaApplication151.java
+++ src/javaapplication151/JavaApplication151.java
@@ -19,7 +19,7 @@ public class JavaApplication151 {
     public static void main(String[] args) {
         ZeneiEljaras ze=new ZeneiEljaras();
         ze.betoltes();
-        ze.kiiro();
+        ze.összkiiro();
     }
     
 }
